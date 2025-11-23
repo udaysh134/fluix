56002e4d474b772abe8c1584c3faffaf0ba296a4
Mohd. Yazdaan Ansari <yazdaanansari31@gmail.com>
2025-11-23 21:32:43 +0530

    Add files via upload
    Check the file for description.

---
+++ src/backend/src/bot.c
@@ -0,0 +1,12 @@
+//file: bot.c
+//Description: File which will integrate QnA, parser aand other essential parsers functions
+//Required files to initiate bot.c: user.c, db.JSON, parser.env
+
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+
+
+//int main(){}
