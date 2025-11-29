ba70fa51219c09be80787c789f88aad09b9ddb41
manikant12aggi <manikantawasthi84@gmail.com>
2025-11-29 19:36:41 +0530

    Update bot.c

--- src/backend/src/bot.c
+++ src/backend/src/bot.c
@@ -6,7 +6,43 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include<windows.h>
+#include <ctype.h>
+
+#include "../include/user.h"
+#include "../packages/cJson/cJSON.h"
+
+/*
+// Declarations
+void createbot();
+void loadbot();
+void editbot();
+
+void editBot(){
+     //this function will update,delete data in the bot database.
+     printf("edit bot");
+     printf("1. update the bot data");
+     printf("2. delete the bot data");
+     int choice;
+     scanf("%d",&choice);
+     switch(choice){
+          case 1:
+               //update the database file
+               break;
+          case 2:
+               //delete the database file
+               break;
+          default:
+               //invalid choice
+               printf("invalid choice");
+               break;
+
+     }
+
+}
 
 
-
-//int main(){}
+int main(){
+    return 0;
+}
+*/
