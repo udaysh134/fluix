58eb679efc6db96cc61bd854c117581f6da5ea5c
Mohd. Yazdaan Ansari <yazdaanansari31@gmail.com>
2025-11-30 15:21:53 +0530

    updated bot.c file
    cleared Manikant's code for more modularity and eligible to be imported once the functions are completed

--- src/backend/src/bot.c
+++ src/backend/src/bot.c
@@ -11,31 +11,55 @@
 
 #include "../include/user.h"
 #include "../packages/cJson/cJSON.h"
+//#include "../include/bot.h"
+//#include ""
 
 /*
 // Declarations
-void createbot();
-void loadbot();
-void editbot();
+void Create_Bot();  //Once coded, this will allow user to create new bots {PENDING}
+void Access_Bot(); //Once coded, this will allow user to access their bots {PENDING}
+void Edit_Bot();  //Once coded, this will allow user to edit their existing bots {PENDING}
+Void Ask_Fluixo(); //Once coded, this will allow user to ask questions to their bot {PENDING}
 
-void editBot(){
-     //this function will update,delete data in the bot database.
-     printf("edit bot");
-     printf("1. update the bot data");
-     printf("2. delete the bot data");
-     int choice;
-     scanf("%d",&choice);
+
+void Access_Bot(){
+
+     // Here, we will give user the choice to whether update, ask questions or delete their bot database
+
+     printf("\t\tWelcome to FLuixo, {USERNAME_HERE}!\n\tWhat's the agenda today?");
+     char choice[7];
+     printf("1. Ignite your curiousity with FLUIXO");
+     printf("\n2. Update your bot  ");
+     printf("\n3. Create a new bot\n")
+   
+     printf("Enter valid choice number from above options: ");
+     fgets(choice,sizeof(choice),stdin);
+
      switch(choice){
-          case 1:
-               //update the database file
+
+          case '1':
+               //Allow user to interact with their bots
+               //Ask_Fluixo();
                break;
-          case 2:
-               //delete the database file
+
+          case '2':
+               //Update existing bot according to user preferences
+               //Edit_Bot();
                break;
+
+          case '3':
+               //Allow user to create new bot
+               //Create_Bot();
+               break;
+          case '4':
+              //Exit from bot access menu
+              printf("Exiting from FLUIXO bot access menu...\n");
+              break;
+              
           default:
                //invalid choice
-               printf("invalid choice");
-               break;
+               printf("Uhh-ohh!!\nYour input choice seems to be invalid :(\nPlease try again :)\n");
+               
 
      }
 
