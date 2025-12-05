cf918c53b4ef4bc95d0ac3a44d396bbbe28a7b4d
SenorDan031 <yazdaanansari31@gmail.com>
2025-12-05 19:01:40 +0530

    test file for creating bot

---
+++ src/backend/tests/create_bot_exp0001.c
@@ -0,0 +1,18 @@
+//create_bot() experiment 0001
+
+#include<stdio.h>
+#include<ctype.h>
+#include<string.h>
+#include<stdlib.h>
+#include<windows.h>
+# include "../packages/cJson/cJSON.h"
+
+/*
+
+int Create_Bot(){
+
+
+
+}
+
+*/
